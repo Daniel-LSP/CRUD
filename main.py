c85b4a053ce1cfb01e23fde7aca33bd05dbedfd8
Daniel-LSP <gcinovation12@gmail.com>
2025-05-16 19:54:16 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, url_for, redirect
 app=Flask(__name__)
 
 usuario=[]
@@ -12,7 +12,7 @@ def crud():
         correo=request.form ["correo"]
         usuario.append({"id": id_contador, "nombre": nombre, "correo": correo})
         id_contador+=1
-        #print(usuario)
+        print(usuario)
     eliminar_id=request.args.get("eliminar")
     if eliminar_id:
         for diccionario in usuario:
@@ -22,13 +22,24 @@ def crud():
 
     return render_template ("crud.html", usuario=usuario)
 
+#Ruta para editar la informacion de un usuario
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    usuario_a_editar=""
+    #TODO: capturar y buscar el usuario a editar
+    for diccionario in usuario: #Para cada diccionario dentro de la lista evalue:
+        if diccionario['id']==id: #Si el id convertido a string es igual al id que me pasan por parametro 
+            usuario_a_editar=diccionario #Hemos identificado los datos del usuario a editar
+            break
 
+    #TODO: actualizar la informacion del usuario seleccionado
+    if request.method=="POST":
+        usuario_a_editar["nombre"]=request.form.get("nombre")#el nombre nuevo sera el que llegue por un nuevo formulario
+        usuario_a_editar["correo"]=request.form.get("correo")#el correo nuevo sera el que llegue por un nuevo formulario
 
+        return redirect(url_for("crud"))#Redireccione la aplicacion a la ruta de la funciom crud
 
-
-
-
-
+    return render_template("editar.html", usuario_a_editar=usuario_a_editar)
 
 
 
